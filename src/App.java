57559cf3c83fb17907b67d3a4d06e07b1be8498a
Viniccius Wirz <vinimovel23@gmail.com>
2024-01-18 14:49:11 -0300

    feat: multiplcation table logic

--- src/App.java
+++ src/App.java
@@ -1,5 +1,14 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        int num = 5;
+
+        int tableLength = 10;
+
+        System.out.println("Tabela de multiplicação por " + num);
+
+        for (int i = 1; i <= tableLength; i++) {
+            int multiplication = num * i;
+            System.out.println(num + " x " + i + " = " + multiplication);
+        }
     }
 }
